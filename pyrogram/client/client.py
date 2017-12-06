79e972df85bb5ddb5e9601165141a99de7b0c310
Dan <14043624+delivrance@users.noreply.github.com>
2017-12-06 21:33:57 +0100

    Move ACTIONS outside the method

--- pyrogram/client/client.py
+++ pyrogram/client/client.py
@@ -454,30 +454,30 @@ class Client:
             )
         )
 
+    ACTIONS = {
+        "cancel": types.SendMessageCancelAction,
+        "typing": types.SendMessageTypingAction,
+        "playing": types.SendMessageGamePlayAction,
+        "choose_contact": types.SendMessageChooseContactAction,
+        "upload_photo": types.SendMessageUploadPhotoAction,
+        "record_video": types.SendMessageRecordVideoAction,
+        "upload_video": types.SendMessageUploadVideoAction,
+        "record_audio": types.SendMessageRecordAudioAction,
+        "upload_audio": types.SendMessageUploadAudioAction,
+        "upload_document": types.SendMessageUploadDocumentAction,
+        "find_location": types.SendMessageGeoLocationAction,
+        "record_video_note": types.SendMessageRecordRoundAction,
+        "upload_video_note": types.SendMessageUploadRoundAction,
+    }
+
     def send_chat_action(self,
                          chat_id: int or str,
                          action: str,
                          progress: int = 0):
-        actions = {
-            "cancel": types.SendMessageCancelAction,
-            "typing": types.SendMessageTypingAction,
-            "playing": types.SendMessageGamePlayAction,
-            "choose_contact": types.SendMessageChooseContactAction,
-            "upload_photo": types.SendMessageUploadPhotoAction,
-            "record_video": types.SendMessageRecordVideoAction,
-            "upload_video": types.SendMessageUploadVideoAction,
-            "record_audio": types.SendMessageRecordAudioAction,
-            "upload_audio": types.SendMessageUploadAudioAction,
-            "upload_document": types.SendMessageUploadDocumentAction,
-            "find_location": types.SendMessageGeoLocationAction,
-            "record_video_note": types.SendMessageRecordRoundAction,
-            "upload_video_note": types.SendMessageUploadRoundAction,
-        }
-
         return self.send(
             functions.messages.SetTyping(
                 peer=self.resolve_peer(chat_id),
-                action=actions.get(
+                action=self.ACTIONS.get(
                     action.lower()
                 )(progress=progress)
             )
