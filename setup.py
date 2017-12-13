a7970cedf57df834655bac37b73030222eb8927a
Dan <14043624+delivrance@users.noreply.github.com>
2017-12-14 00:13:51 +0100

    Fix logo not showing up on PyPI website

--- setup.py
+++ setup.py
@@ -31,6 +31,6 @@ if len(argv) > 1 and argv[1] != "sdist":
 # PyPI doesn't like raw html
 with open("README.rst", encoding="UTF-8") as f:
     readme = re.sub(r"\.\. \|.+\| raw:: html(?:\s{4}.+)+\n\n", "", f.read())
-    readme = re.sub(r"\|header\|", "|logo|\n######\n\n|description|\n\n|scheme| |mtproto|", readme)
+    readme = re.sub(r"\|header\|", "|logo|\n\n|description|\n\n|scheme| |mtproto|", readme)
 
 setup(long_description=readme)
