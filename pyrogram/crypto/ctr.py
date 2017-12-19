b35fabd9d59f681f1a22e7678198f372492c4177
Dan <14043624+delivrance@users.noreply.github.com>
2017-12-19 13:59:15 +0100

    Add AES CTR operation mode

--- pyrogram/crypto/ctr.py
+++ pyrogram/crypto/ctr.py
@@ -16,8 +16,20 @@
 # You should have received a copy of the GNU Lesser General Public License
 # along with Pyrogram.  If not, see <http://www.gnu.org/licenses/>.
 
-from .ctr import CTR
-from .ige import IGE
-from .kdf import KDF
-from .prime import Prime
-from .rsa import RSA
+try:
+    from pyaes import AESModeOfOperationCTR
+except ImportError:
+    pass
+
+
+class CTR:
+    def __init__(self, key: bytes, iv: bytes):
+        self.ctr = AESModeOfOperationCTR(key)
+        self.iv = iv
+
+    def decrypt(self, data: bytes, offset: int) -> bytes:
+        replace = int.to_bytes(offset // 16, byteorder="big", length=4)
+        iv = self.iv[:-4] + replace
+        self.ctr._counter._counter = list(iv)
+
+        return self.ctr.decrypt(data)
