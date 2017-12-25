2d9579945b88d09fcddf4c8af76d666e0fab11a7
Dan <14043624+delivrance@users.noreply.github.com>
2017-12-25 13:45:16 +0100

    Update to v0.2.2

--- pyrogram/__init__.py
+++ pyrogram/__init__.py
@@ -18,7 +18,7 @@
 
 __copyright__ = "Copyright (C) 2017 Dan Tès <https://github.com/delivrance>"
 __license__ = "GNU Lesser General Public License v3 or later (LGPLv3+)"
-__version__ = "0.2.1"
+__version__ = "0.2.2"
 
 from .client import ChatAction
 from .client import Client
