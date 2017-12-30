74788f934f36b59e47d9dcef5f25f58c3d97526a
Dan <14043624+delivrance@users.noreply.github.com>
2017-12-30 19:31:22 +0100

    Expose the Error class from the main package

--- pyrogram/__init__.py
+++ pyrogram/__init__.py
@@ -20,5 +20,6 @@ __copyright__ = "Copyright (C) 2017 Dan Tès <https://github.com/delivrance>"
 __license__ = "GNU Lesser General Public License v3 or later (LGPLv3+)"
 __version__ = "0.2.2"
 
+from .api.errors import Error
 from .client import ChatAction
 from .client import Client
