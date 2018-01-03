b06ae1c38e5a4b96fa1146d19ce4b330f41c0c00
Dan <14043624+delivrance@users.noreply.github.com>
2018-01-03 19:10:40 +0100

    Update to v0.3.2

--- pyrogram/__init__.py
+++ pyrogram/__init__.py
@@ -18,7 +18,7 @@
 
 __copyright__ = "Copyright (C) 2017-2018 Dan Tès <https://github.com/delivrance>"
 __license__ = "GNU Lesser General Public License v3 or later (LGPLv3+)"
-__version__ = "0.3.0"
+__version__ = "0.3.2"
 
 from .api.errors import Error
 from .client import ChatAction
