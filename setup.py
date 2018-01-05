a926561f76a19aa47a7a11e2972aa9b42e1368ab
Dan <14043624+delivrance@users.noreply.github.com>
2018-01-05 02:16:55 +0100

    Generate docs source on setup

--- setup.py
+++ setup.py
@@ -23,10 +23,12 @@ from setuptools import setup
 
 from compiler.api import compiler as api_compiler
 from compiler.error import compiler as error_compiler
+from compiler.docs import compiler as docs_compiler
 
 if len(argv) > 1 and argv[1] != "sdist":
     api_compiler.start()
     error_compiler.start()
+    docs_compiler.start()
 
 # PyPI doesn't like raw html
 with open("README.rst", encoding="UTF-8") as f:
