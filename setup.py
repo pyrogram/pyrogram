0dd5843473699afa20650a6abf669289030f7219
Dan <14043624+delivrance@users.noreply.github.com>
2018-01-28 00:56:12 +0100

    Remove TgCrypto from required packages

--- setup.py
+++ setup.py
@@ -67,6 +67,6 @@ setup(
     ],
     packages=find_packages(),
     zip_safe=False,
-    install_requires=["pyaes", "pysocks", "tgcrypto"],
+    install_requires=["pyaes", "pysocks"],
     include_package_data=True,
 )
