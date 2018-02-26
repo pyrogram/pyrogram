eada298e8cd1f23bc6d66a05496029acf9df0793
Dan <14043624+delivrance@users.noreply.github.com>
2018-02-26 15:09:24 +0100

    Add hello_world example

---
+++ examples/hello_world.py
@@ -0,0 +1,19 @@
+from pyrogram import Client
+
+# Create a new Client
+client = Client("example")
+
+# Start the Client
+client.start()
+
+# Send a message to yourself, Markdown is enabled by default
+client.send_message("me", "Hi there! I'm using **Pyrogram**")
+
+# Send a photo with a formatted caption to yourself
+client.send_photo("me", "pyrogram.png", "__This is a formatted__ **caption**")
+
+# Send a location to yourself
+client.send_location("me", 51.500729, -0.124583)
+
+# Stop the client
+client.stop()
