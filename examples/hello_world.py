0219ac0b5a3388598ce455aaabb1f52d3e9c3cf2
Dan <14043624+delivrance@users.noreply.github.com>
2018-02-26 15:51:31 +0100

    Move non-code files into data folder

--- examples/hello_world.py
+++ examples/hello_world.py
@@ -10,7 +10,7 @@ client.start()
 client.send_message("me", "Hi there! I'm using **Pyrogram**")
 
 # Send a photo with a formatted caption to yourself
-client.send_photo("me", "pyrogram.png", "__This is a formatted__ **caption**")
+client.send_photo("me", "data/pyrogram.png", "__This is a formatted__ **caption**")
 
 # Send a location to yourself
 client.send_location("me", 51.500729, -0.124583)
