48bf7438b71b48bfa4bd766677a59ebe6e192aa7
Dan <14043624+delivrance@users.noreply.github.com>
2018-03-09 14:45:06 +0100

    Add PhotoSize type

---
+++ pyrogram/client/types/photo_size.py
@@ -0,0 +1,10 @@
+class PhotoSize:
+    def __init__(self,
+                 file_id: str,
+                 width: int,
+                 height: int,
+                 file_size: int):
+        self.file_id = file_id
+        self.width = width
+        self.height = height
+        self.file_size = file_size
