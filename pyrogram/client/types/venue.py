24c2d20137c3601f8d835eb483f8536ead6c4cf1
Dan <14043624+delivrance@users.noreply.github.com>
2018-03-09 14:46:16 +0100

    Add Venue type

---
+++ pyrogram/client/types/venue.py
@@ -0,0 +1,13 @@
+from . import Location
+
+
+class Venue:
+    def __init__(self,
+                 location: Location,
+                 title: str,
+                 address: str,
+                 foursquare_id: str = None):
+        self.location = location
+        self.title = title
+        self.address = address
+        self.foursquare_id = foursquare_id
