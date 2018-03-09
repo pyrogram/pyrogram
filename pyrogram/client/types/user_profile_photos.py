345da2fccdfd6ccb0269c27f96a3aff83b4dcf5f
Dan <14043624+delivrance@users.noreply.github.com>
2018-03-09 14:47:12 +0100

    Add UserProfilePhotos type

---
+++ pyrogram/client/types/user_profile_photos.py
@@ -0,0 +1,6 @@
+class UserProfilePhotos:
+    def __init__(self,
+                 total_count: int,
+                 photos: list):
+        self.total_count = total_count
+        self.photos = photos
