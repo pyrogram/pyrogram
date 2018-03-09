35fcfe8266d93a353a5ea8f7235d56c810f4cc5a
Dan <14043624+delivrance@users.noreply.github.com>
2018-03-09 14:45:58 +0100

    Add Sticker type

---
+++ pyrogram/client/types/sticker.py
@@ -0,0 +1,21 @@
+from . import PhotoSize, MaskPosition
+
+
+class Sticker:
+    def __init__(self,
+                 file_id: str,
+                 width: int,
+                 height: int,
+                 thumb: PhotoSize = None,
+                 emoji: str = None,
+                 set_name: str = None,
+                 mask_position: MaskPosition = None,
+                 file_size: int = None):
+        self.file_id = file_id
+        self.width = width
+        self.height = height
+        self.thumb = thumb
+        self.emoji = emoji
+        self.set_name = set_name
+        self.mask_position = mask_position
+        self.file_size = file_size
