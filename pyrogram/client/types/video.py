7ff9f28e153969279ea18affc3af1d9664af5129
Dan <14043624+delivrance@users.noreply.github.com>
2018-03-09 14:47:45 +0100

    Add Video type

---
+++ pyrogram/client/types/video.py
@@ -0,0 +1,19 @@
+from . import PhotoSize
+
+
+class Video:
+    def __init__(self,
+                 file_id: str,
+                 width: int,
+                 height: int,
+                 duration: int,
+                 thumb: PhotoSize = None,
+                 mime_type: str = None,
+                 file_size: int = None):
+        self.file_id = file_id
+        self.width = width
+        self.height = height
+        self.duration = duration
+        self.thumb = thumb
+        self.mime_type = mime_type
+        self.file_size = file_size
