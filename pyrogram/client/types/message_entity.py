4e4d9e6e44c0d74401c143ed07762d1ab8ceee29
Dan <14043624+delivrance@users.noreply.github.com>
2018-03-09 14:45:18 +0100

    Add MessageEntity type

---
+++ pyrogram/client/types/message_entity.py
@@ -0,0 +1,15 @@
+from . import User
+
+
+class MessageEntity:
+    def __init__(self,
+                 type: str,
+                 offset: int,
+                 length: int,
+                 url: str = None,
+                 user: User = None):
+        self.type = type
+        self.offset = offset
+        self.length = length
+        self.url = url
+        self.user = user
