40e7d72e873239b7f11d88ce0b8345010627a01e
Dan <14043624+delivrance@users.noreply.github.com>
2018-03-21 15:43:58 +0100

    Make paths good looking

--- pyrogram/client/client.py
+++ pyrogram/client/client.py
@@ -573,7 +573,7 @@ class Client:
                         )
 
                 if temp_file_path:
-                    final_file_path = os.path.join(directory, file_name)
+                    final_file_path = re.sub("\\\\", "/", os.path.join(directory, file_name))
                     os.makedirs(directory, exist_ok=True)
                     shutil.move(temp_file_path, final_file_path)
             except Exception as e:
