bf0b8aa692d069c0c3aea0bbcf6cc82a15196d88
Dan <14043624+delivrance@users.noreply.github.com>
2018-03-21 18:43:48 +0100

    Clean code

--- setup.py
+++ setup.py
@@ -24,12 +24,9 @@ from setuptools import setup, find_packages
 from compiler.api import compiler as api_compiler
 from compiler.error import compiler as error_compiler
 
-# from compiler.docs import compiler as docs_compiler
-
 if len(argv) > 1 and argv[1] != "sdist":
     api_compiler.start()
     error_compiler.start()
-    # docs_compiler.start()
 
 with open("pyrogram/__init__.py", encoding="utf-8") as f:
     version = re.findall(r"__version__ = \"(.+)\"", f.read())[0]
