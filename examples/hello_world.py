2e604572956d92bfebb4996d048834b9d0fc5446
Dan <14043624+delivrance@users.noreply.github.com>
2018-04-14 16:12:52 +0200

    Remove data folder

--- examples/hello_world.py
+++ examples/hello_world.py
@@ -9,9 +9,6 @@ client.start()
 # Send a message to yourself, Markdown is enabled by default
 client.send_message("me", "Hi there! I'm using **Pyrogram**")
 
-# Send a photo with a formatted caption to yourself
-client.send_photo("me", "data/pyrogram.png", "__This is a formatted__ **caption**")
-
 # Send a location to yourself
 client.send_location("me", 51.500729, -0.124583)
 
