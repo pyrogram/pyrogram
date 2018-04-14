849b42b444f5d781c020350e569c49ec309ec178
Dan <14043624+delivrance@users.noreply.github.com>
2018-04-14 16:13:24 +0200

    Update hello_world.py

--- examples/hello_world.py
+++ examples/hello_world.py
@@ -1,16 +1,16 @@
 from pyrogram import Client
 
 # Create a new Client
-client = Client("example")
+app = Client("my_account")
 
 # Start the Client
-client.start()
+app.start()
 
 # Send a message to yourself, Markdown is enabled by default
-client.send_message("me", "Hi there! I'm using **Pyrogram**")
+app.send_message("me", "Hi there! I'm using **Pyrogram**")
 
 # Send a location to yourself
-client.send_location("me", 51.500729, -0.124583)
+app.send_location("me", 51.500729, -0.124583)
 
 # Stop the client
-client.stop()
+app.stop()
