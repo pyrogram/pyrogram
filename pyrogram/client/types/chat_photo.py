c457fc6ae3e6da7e5c98fb142c9b2b45a54c3ec3
Dan <14043624+delivrance@users.noreply.github.com>
2018-04-24 15:07:32 +0200

    Clean ChatPhoto type

--- pyrogram/client/types/chat_photo.py
+++ pyrogram/client/types/chat_photo.py
@@ -35,6 +35,6 @@ class ChatPhoto(Object):
     """
     ID = 0xb0700015
 
-    def __init__(self, small_file_id, big_file_id):
+    def __init__(self, small_file_id: str, big_file_id: str):
         self.small_file_id = small_file_id  # string
         self.big_file_id = big_file_id  # string
