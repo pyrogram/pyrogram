c32e1d9444650b8ca5e3603a4edf34026b35fea6
Dan <14043624+delivrance@users.noreply.github.com>
2018-04-29 18:37:03 +0200

    Document answer_callback_query

--- pyrogram/client/client.py
+++ pyrogram/client/client.py
@@ -3594,6 +3594,30 @@ class Client:
                               show_alert: bool = None,
                               url: str = None,
                               cache_time: int = 0):
+        """Use this method to send answers to callback queries sent from inline keyboards.
+        The answer will be displayed to the user as a notification at the top of the chat screen or as an alert.
+
+        Args:
+            callback_query_id (``str``):
+                Unique identifier for the query to be answered.
+
+            text (``str``):
+                Text of the notification. If not specified, nothing will be shown to the user, 0-200 characters.
+
+            show_alert (``bool``):
+                If true, an alert will be shown by the client instead of a notification at the top of the chat screen.
+                Defaults to False.
+
+            url (``str``):
+                URL that will be opened by the user's client.
+                If you have created a Game and accepted the conditions via @Botfather, specify the URL that opens your
+                game – note that this will only work if the query comes from a callback_game button.
+                Otherwise, you may use links like t.me/your_bot?start=XXXX that open your bot with a parameter.
+
+            cache_time (``int``):
+                The maximum amount of time in seconds that the result of the callback query may be cached client-side.
+                Telegram apps will support caching starting in version 3.14. Defaults to 0.
+        """
         return self.send(
             functions.messages.SetBotCallbackAnswer(
                 query_id=int(callback_query_id),
