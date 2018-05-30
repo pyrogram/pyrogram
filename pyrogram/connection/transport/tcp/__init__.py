98ebad34281aa082cab414027e0e46740452d885
Dan <14043624+delivrance@users.noreply.github.com>
2018-05-30 19:29:03 +0200

    Add TCP Intermediate obfuscated protocol

--- pyrogram/connection/transport/tcp/__init__.py
+++ pyrogram/connection/transport/tcp/__init__.py
@@ -20,3 +20,4 @@ from .tcp_abridged import TCPAbridged
 from .tcp_abridged_o import TCPAbridgedO
 from .tcp_full import TCPFull
 from .tcp_intermediate import TCPIntermediate
+from .tcp_intermediate_o import TCPIntermediateO
---
+++ pyrogram/connection/transport/tcp/tcp_intermediate_o.py
@@ -0,0 +1,81 @@
+# Pyrogram - Telegram MTProto API Client Library for Python
+# Copyright (C) 2017-2018 Dan Tès <https://github.com/delivrance>
+#
+# This file is part of Pyrogram.
+#
+# Pyrogram is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published
+# by the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Pyrogram is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with Pyrogram.  If not, see <http://www.gnu.org/licenses/>.
+
+import logging
+import os
+from struct import pack, unpack
+
+from .tcp import TCP
+from ....crypto.aes import AES
+
+log = logging.getLogger(__name__)
+
+
+class TCPIntermediateO(TCP):
+    RESERVED = (b"HEAD", b"POST", b"GET ", b"OPTI", b"\xee" * 4)
+
+    def __init__(self, proxy: dict):
+        super().__init__(proxy)
+        self.encrypt = None
+        self.decrypt = None
+
+    def connect(self, address: tuple):
+        super().connect(address)
+
+        while True:
+            nonce = bytearray(os.urandom(64))
+
+            if (nonce[0] != b"\xef"
+                    and nonce[:4] not in self.RESERVED
+                    and nonce[4:4] != b"\x00" * 4):
+                nonce[56] = nonce[57] = nonce[58] = nonce[59] = 0xee
+                break
+
+        temp = bytearray(nonce[55:7:-1])
+
+        self.encrypt = (nonce[8:40], nonce[40:56], bytearray(1))
+        self.decrypt = (temp[0:32], temp[32:48], bytearray(1))
+
+        nonce[56:64] = AES.ctr256_encrypt(nonce, *self.encrypt)[56:64]
+
+        super().sendall(nonce)
+
+        log.info("Connected{}!".format(" with proxy" if self.proxy_enabled else ""))
+
+    def sendall(self, data: bytes, *args):
+        super().sendall(
+            AES.ctr256_encrypt(
+                pack("<i", len(data)) + data,
+                *self.encrypt
+            )
+        )
+
+    def recvall(self, length: int = 0) -> bytes or None:
+        length = super().recvall(4)
+
+        if length is None:
+            return None
+
+        length = AES.ctr256_decrypt(length, *self.decrypt)
+
+        data = super().recvall(unpack("<i", length)[0])
+
+        if data is None:
+            return None
+
+        return AES.ctr256_decrypt(data, *self.decrypt)
