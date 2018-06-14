2cf930bea08ed71392a84c0332800fd7352ea930
Dan <14043624+delivrance@users.noreply.github.com>
2018-06-14 03:24:39 +0200

    Remove commented MTProto methods

--- pyrogram/session/session.py
+++ pyrogram/session/session.py
@@ -205,47 +205,8 @@ class Session:
         log.debug("Session stopped")
 
     async def restart(self):
-        await self.stop()
-        await self.start()
-
-    def pack(self, message: Message):
-        data = Long(self.current_salt.salt) + self.session_id + message.write()
-        padding = urandom(-(len(data) + 12) % 16 + 12)
-
-    # def pack(self, message: Message):
-    #     data = Long(self.current_salt.salt) + self.session_id + message.write()
-    #     padding = urandom(-(len(data) + 12) % 16 + 12)
-    #
-    #     # 88 = 88 + 0 (outgoing message)
-    #     msg_key_large = sha256(self.auth_key[88: 88 + 32] + data + padding).digest()
-    #     msg_key = msg_key_large[8:24]
-    #     aes_key, aes_iv = KDF(self.auth_key, msg_key, True)
-    #
-    #     return self.auth_key_id + msg_key + AES.ige256_encrypt(data + padding, aes_key, aes_iv)
-    #
-    # def unpack(self, b: BytesIO) -> Message:
-    #     assert b.read(8) == self.auth_key_id, b.getvalue()
-    #
-    #     msg_key = b.read(16)
-    #     aes_key, aes_iv = KDF(self.auth_key, msg_key, False)
-    #     data = BytesIO(AES.ige256_decrypt(b.read(), aes_key, aes_iv))
-    #     data.read(8)
-    #
-    #     # https://core.telegram.org/mtproto/security_guidelines#checking-session-id
-    #     assert data.read(8) == self.session_id
-    #
-    #     message = Message.read(data)
-    #
-    #     # https://core.telegram.org/mtproto/security_guidelines#checking-sha256-hash-value-of-msg-key
-    #     # https://core.telegram.org/mtproto/security_guidelines#checking-message-length
-    #     # 96 = 88 + 8 (incoming message)
-    #     assert msg_key == sha256(self.auth_key[96:96 + 32] + data.getvalue()).digest()[8:24]
-    #
-    #     # https://core.telegram.org/mtproto/security_guidelines#checking-msg-id
-    #     # TODO: check for lower msg_ids
-    #     assert message.msg_id % 2 != 0
-    #
-    #     return message
+        self.stop()
+        self.start()
 
     async def net_worker(self):
         name = threading.current_thread().name
