2b36fb31d94262a6f43538c23286ec15ff505284
Dan <14043624+delivrance@users.noreply.github.com>
2018-07-04 21:03:29 +0200

    Add Dialog type

---
+++ pyrogram/client/types/dialog.py
@@ -0,0 +1,28 @@
+# Pyrogram - Telegram MTProto API Client Library for Python
+# Copyright (C) 2017-2018 Dan Tès <https://github.com/delivrance>
+#
+# This file is part of Pyrogram.
+#
+# Pyrogram is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published
+# by the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Pyrogram is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with Pyrogram.  If not, see <http://www.gnu.org/licenses/>.
+
+from pyrogram.api.core import Object
+
+
+class Dialog(Object):
+    ID = 0xb0700028
+
+    def __init__(self, id: int, top_message):
+        # TODO docstrings
+        self.id = id
+        self.top_message = top_message
