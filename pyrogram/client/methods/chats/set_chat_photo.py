cb4996d64bac7ea57ce98784af76e3ca038ec1f1
Dan <14043624+delivrance@users.noreply.github.com>
2018-07-08 10:18:48 +0200

    Reformat code

--- pyrogram/client/methods/chats/set_chat_photo.py
+++ pyrogram/client/methods/chats/set_chat_photo.py
@@ -15,6 +15,7 @@
 #
 # You should have received a copy of the GNU Lesser General Public License
 # along with Pyrogram.  If not, see <http://www.gnu.org/licenses/>.
+
 import os
 from base64 import b64decode
 from struct import unpack
