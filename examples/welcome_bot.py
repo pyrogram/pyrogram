c2da2a61ece6711bd7ed51d68249995843ff5dec
Dan <14043624+delivrance@users.noreply.github.com>
2018-10-09 16:37:53 +0200

    Update welcome_bot.py

--- examples/welcome_bot.py
+++ examples/welcome_bot.py
@@ -1,10 +1,13 @@
 """This is the Welcome Bot in @PyrogramChat.
 
 It uses the Emoji module to easily add emojis in your text messages and Filters
-to make it only work for specific messages in a specific chat
+to make it only work for specific messages in a specific chat.
 """
 
 from pyrogram import Client, Emoji, Filters
+
+MENTION = "[{}](tg://user?id={})"
+MESSAGE = "{} Welcome to [Pyrogram](https://docs.pyrogram.ml/)'s group chat {}!"
 
 app = Client("my_account")
 
@@ -12,23 +15,13 @@ app = Client("my_account")
 @app.on_message(Filters.chat("PyrogramChat") & Filters.new_chat_members)
 def welcome(client, message):
     # Build the new members list (with mentions) by using their first_name
-    new_members = ", ".join([
-        "[{}](tg://user?id={})".format(i.first_name, i.id)
-        for i in message.new_chat_members
-    ])
+    new_members = [MENTION.format(i.first_name, i.id) for i in message.new_chat_members]
 
     # Build the welcome message by using an emoji and the list we built above
-    text = "{} Welcome to [Pyrogram](https://docs.pyrogram.ml/)'s group chat {}!".format(
-        Emoji.SPARKLES,
-        new_members
-    )
+    text = MESSAGE.format(Emoji.SPARKLES, ", ".join(new_members))
 
     # Send the welcome message
-    client.send_message(
-        message.chat.id, text,
-        reply_to_message_id=message.message_id,
-        disable_web_page_preview=True
-    )
+    message.reply(text, disable_web_page_preview=True)
 
 
 app.run()  # Automatically start() and idle()
