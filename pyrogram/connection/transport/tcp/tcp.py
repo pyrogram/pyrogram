0c767742839ad8f8229efbe69d8e9e4bd53952fa
Dan <14043624+delivrance@users.noreply.github.com>
2018-11-02 10:26:59 +0100

    Reformat code

--- pyrogram/connection/transport/tcp/tcp.py
+++ pyrogram/connection/transport/tcp/tcp.py
@@ -16,9 +16,9 @@
 # You should have received a copy of the GNU Lesser General Public License
 # along with Pyrogram.  If not, see <http://www.gnu.org/licenses/>.
 
+import ipaddress
 import logging
 import socket
-import ipaddress
 
 try:
     import socks
