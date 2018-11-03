57b7432e328a3e12246b4997528272156c36fb8d
Furoin <furoin@protonmail.com>
2018-11-03 21:03:59 +0300

    added message.is_mentioned

--- pyrogram/client/ext/utils.py
+++ pyrogram/client/ext/utils.py
@@ -591,6 +591,8 @@ def parse_messages(
                 else:
                     reply_markup = None
 
+            is_mentioned = message.mentioned
+
             m = pyrogram_types.Message(
                 message_id=message.id,
                 date=message.date,
@@ -606,6 +608,7 @@ def parse_messages(
                 forward_from_message_id=forward_from_message_id,
                 forward_signature=forward_signature,
                 forward_date=forward_date,
+                is_mentioned=is_mentioned,
                 edit_date=message.edit_date,
                 media_group_id=message.grouped_id,
                 photo=photo,
@@ -1052,3 +1055,4 @@ def parse_chat_members(members: types.channels.ChannelParticipants or types.mess
             total_count=len(members),
             chat_members=parsed_members
         )
+
