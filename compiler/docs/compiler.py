6982c436a88e88ef78124d5165e06560c834cec2
Dan <14043624+delivrance@users.noreply.github.com>
2019-08-13 13:18:01 +0200

    Add the new auth-related methods and types to docs

--- compiler/docs/compiler.py
+++ compiler/docs/compiler.py
@@ -128,10 +128,10 @@ def pyrogram_api():
         utilities="""
         Utilities
             start
-            stop
-            restart
             idle
+            stop
             run
+            restart
             add_handler
             remove_handler
             stop_transmission
@@ -247,6 +247,22 @@ def pyrogram_api():
             set_game_score
             get_game_high_scores
         """,
+        authorization="""
+        Authorization
+            connect
+            disconnect
+            initialize
+            terminate
+            send_code
+            resend_code
+            sign_in
+            sign_up
+            get_password_hint
+            check_password
+            send_recovery_code
+            recover_password
+            accept_terms_of_service
+        """,
         advanced="""
         Advanced
             send
@@ -347,6 +363,11 @@ def pyrogram_api():
         InputMessageContent
             InputMessageContent
             InputTextMessageContent
+        """,
+        authorization="""
+        Authorization
+            SentCode
+            TermsOfService
         """
     )
 
