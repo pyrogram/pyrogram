531423ae1c04229f9ce8c6df9d26c9a174581176
Dan <14043624+delivrance@users.noreply.github.com>
2020-02-01 14:17:32 +0100

    Update copyright year

--- pyrogram/__init__.py
+++ pyrogram/__init__.py
@@ -18,7 +18,7 @@
 
 __version__ = "0.16.0"
 __license__ = "GNU Lesser General Public License v3 or later (LGPLv3+)"
-__copyright__ = "Copyright (C) 2017-2019 Dan <https://github.com/delivrance>"
+__copyright__ = "Copyright (C) 2017-2020 Dan <https://github.com/delivrance>"
 
 from .client import *
 from .client.handlers import *
