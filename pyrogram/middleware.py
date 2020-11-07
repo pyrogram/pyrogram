694e4eaf9a67b1278f3f3026396e0b30446bd84f
LK <->
2020-11-07 21:29:05 +0300

    * Trying to fix circular imports

--- pyrogram/middleware.py
+++ pyrogram/middleware.py
@@ -5,5 +5,5 @@ from typing import Any, Callable, Coroutine, TypeVar
 # UpdateType = TypeVar('UpdateType', bound=pyrogram.types.Update)
 # ClientType = TypeVar('ClientType', bound=pyrogram.Client)
 
-CallNextMiddlewareCallback = Callable[[Any, Any], Coroutine[Any]]
-Middleware = Callable[[Any, Any, CallNextMiddlewareCallback], Coroutine[Any]]
+CallNextMiddlewareCallback = Callable[[Any, Any], Coroutine[Any, Any, Any]]
+Middleware = Callable[[Any, Any, CallNextMiddlewareCallback], Coroutine[Any, Any, Any]]
