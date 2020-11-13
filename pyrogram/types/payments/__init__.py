4bb967f2c13fc4e2874c95a96099f73e1e2f25cf
CyanBook <lorenzo.delmonte05@gmail.com>
2020-11-13 12:21:13 +0100

    Fix payment types not available
    Added:
    types/payments/__init__.py
    Added every payment type available
    
    Modified:
    types/__init__.py
    Added payments/__init__.py import

--- pyrogram/types/payments/__init__.py
+++ pyrogram/types/payments/__init__.py
@@ -16,14 +16,8 @@
 #  You should have received a copy of the GNU Lesser General Public License
 #  along with Pyrogram.  If not, see <http://www.gnu.org/licenses/>.
 
-from .authorization import *
-from .bots_and_keyboards import *
-from .inline_mode import *
-from .input_media import *
-from .input_message_content import *
-from .list import List
-from .messages_and_media import *
-from .object import Object
-from .payments import *
-from .update import *
-from .user_and_chats import *
+from .invoice import Invoice
+from .labeled_price import LabeledPrice
+from .media_invoice import MediaInvoice
+
+__all__ = ["Invoice", "LabeledPrice", "MediaInvoice"]
