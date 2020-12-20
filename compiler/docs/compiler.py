e1dac6c0e241c6332024aa0d3c6b9a7e84ae67d2
Legenda24 <48826466+Legenda24@users.noreply.github.com>
2020-12-20 21:02:54 +0500

    Add new method get_media_group (#550)
    * Update __init__.py
    
    Support for GetMediaGroup
    
    * Create get_media_group.py
    
    Added new method - get_media_group
    
    * Update get_media_group.py
    
    Add pyro stuff
    
    * Update get_media_group.py
    
    * Update compiler.py
    
    Co-authored-by: Dan <14043624+delivrance@users.noreply.github.com>

--- compiler/docs/compiler.py
+++ compiler/docs/compiler.py
@@ -168,6 +168,7 @@ def pyrogram_api():
             send_chat_action
             delete_messages
             get_messages
+            get_media_group
             get_history
             get_history_count
             read_history
