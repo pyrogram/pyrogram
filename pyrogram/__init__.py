38efceefb15a720811b52a579d5731763676bf34
Dan <14043624+delivrance@users.noreply.github.com>
2020-12-27 21:08:47 +0100

    Update Pyrogram to v1.1.7

--- pyrogram/__init__.py
+++ pyrogram/__init__.py
@@ -16,7 +16,7 @@
 #  You should have received a copy of the GNU Lesser General Public License
 #  along with Pyrogram.  If not, see <http://www.gnu.org/licenses/>.
 
-__version__ = "1.1.6"
+__version__ = "1.1.7"
 __license__ = "GNU Lesser General Public License v3 or later (LGPLv3+)"
 __copyright__ = "Copyright (C) 2017-2020 Dan <https://github.com/delivrance>"
 
