d483c29b95ab152bd612601c950faffa0e6d16c3
TAOTAO5 <48826173+TAOTAO5@users.noreply.github.com>
2021-03-27 17:54:16 +0800

    Update __init__.py
    add DeleteAccount

--- pyrogram/methods/users/__init__.py
+++ pyrogram/methods/users/__init__.py
@@ -28,6 +28,7 @@ from .set_profile_photo import SetProfilePhoto
 from .unblock_user import UnblockUser
 from .update_profile import UpdateProfile
 from .update_username import UpdateUsername
+from .delete_account import DeleteAccount
 
 
 class Users(
@@ -43,5 +44,6 @@ class Users(
     IterProfilePhotos,
     UnblockUser,
     UpdateProfile,
+    DeleteAccount,
 ):
     pass
