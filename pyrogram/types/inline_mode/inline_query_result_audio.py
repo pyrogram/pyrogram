b90a79fe13ade8d3716f3823eef509fc1df9cd8b
Dan <14043624+delivrance@users.noreply.github.com>
2021-05-12 08:46:47 +0200

    Update inline_query_result_audio.py

--- pyrogram/types/inline_mode/inline_query_result_audio.py
+++ pyrogram/types/inline_mode/inline_query_result_audio.py
@@ -26,39 +26,45 @@ from pyrogram.types import InlineQueryResult
 
 class InlineQueryResultAudio(InlineQueryResult):
     """Link to an audio file.
+    
     By default, this audio file will be sent by the user with optional caption.
     Alternatively, you can use *input_message_content* to send a message with the specified content instead of the
     audio.
+    
     Parameters:
         audio_url (``str``):
             A valid URL for the audio file.
+            
         title (``str``):
             Title for the result.
-        duration (``int``, *optional*):
-            Duration of the audio in second.
-        voice (``bool``, *optional*):
-            Whether this file should be sent as voice message (default=False).
-        performer (``str``, *optional*):
-            Audio artist.
-        thumb_url (``str``, *optional*):
-            URL of the static thumbnail for the result (jpeg or gif)
-            Defaults to the value passed in *animation_url*.
+            
         id (``str``, *optional*):
             Unique identifier for this result, 1-64 bytes.
             Defaults to a randomly generated UUID4.
-        description (``str``, *optional*):
-            Short description of the result.
+            
+        performer (``str``, *optional*):
+            Audio performer.
+            
+        audio_duration (``int``, *optional*):
+            Audio duration in seconds.
+
         caption (``str``, *optional*):
             Caption of the photo to be sent, 0-1024 characters.
+            
         parse_mode (``str``, *optional*):
             By default, texts are parsed using both Markdown and HTML styles.
             You can combine both syntaxes together.
             Pass "markdown" or "md" to enable Markdown-style parsing only.
             Pass "html" to enable HTML-style parsing only.
             Pass None to completely disable style parsing.
+            
+        caption_entities (List of :obj:`~pyrogram.types.MessageEntity`):
+            List of special entities that appear in the caption, which can be specified instead of *parse_mode*.
+            
         reply_markup (:obj:`~pyrogram.types.InlineKeyboardMarkup`, *optional*):
-            An InlineKeyboardMarkup object.
-        input_message_content (:obj:`~pyrogram.types.InputMessageContent`):
+            Inline keyboard attached to the message.
+            
+        input_message_content (:obj:`~pyrogram.types.InputMessageContent`, *optional*):
             Content of the message to be sent instead of the photo.
     """
 
@@ -66,63 +72,53 @@ class InlineQueryResultAudio(InlineQueryResult):
             self,
             audio_url: str,
             title: str,
-            duration: int = 0,
-            voice: bool = False,
-            performer: str = "",
-            thumb_url: str = None,
             id: str = None,
-            description: str = None,
+            performer: str = "",
+            audio_duration: int = 0,
             caption: str = "",
             parse_mode: Union[str, None] = object,
+            caption_entities: List["types.MessageEntity"] = None,
             reply_markup: "types.InlineKeyboardMarkup" = None,
             input_message_content: "types.InputMessageContent" = None
     ):
         super().__init__("audio", id, input_message_content, reply_markup)
 
         self.audio_url = audio_url
-        self.thumb_url = thumb_url
         self.title = title
-        self.description = description
+        self.performer = performer
+        self.audio_duration = audio_duration
         self.caption = caption
         self.parse_mode = parse_mode
-        self.reply_markup = reply_markup
-        self.input_message_content = input_message_content
-        self.duration = duration
-        self.voice = voice
-        self.performer = performer
+        self.caption_entities = caption_entities
 
     async def write(self):
         audio = raw.types.InputWebDocument(
             url=self.audio_url,
             size=0,
-            mime_type="audio/ogg",
+            mime_type="audio/mpeg",
             attributes=[raw.types.DocumentAttributeAudio(
-                duration=self.duration,
-                voice=self.voice,
+                duration=self.audio_duration,
                 title=self.title,
-                performer=self.performer,
-                # waveform=idk wtf is that...
+                performer=self.performer
             )],
         )
+        
+        message, entities = (await utils.parse_text_entities(
+            client, self.caption, self.parse_mode, self.caption_entities
+        )).values()
 
         return raw.types.InputBotInlineResult(
             id=self.id,
             type=self.type,
             title=self.title,
-            description=self.description,
-            thumb=raw.types.InputWebDocument(
-                url=self.thumb_url,
-                size=0,
-                mime_type="image/jpeg",
-                attributes=[]
-            ) if self.thumb_url else None,
             content=audio,
             send_message=(
-                self.input_message_content.write(self.reply_markup)
+                await self.input_message_content.write(client, self.reply_markup)
                 if self.input_message_content
                 else raw.types.InputBotInlineMessageMediaAuto(
-                    reply_markup=self.reply_markup.write() if self.reply_markup else None,
-                    **await(Parser(None)).parse(self.caption, self.parse_mode)
+                    reply_markup=await self.reply_markup.write(client) if self.reply_markup else None,
+                    message=message,
+                    entities=entities
                 )
             )
         )
