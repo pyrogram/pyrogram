8fcc536cf38e13fae3747f7ebb047e63b34afda5
Shrimadhav U K <SpEcHiDe@users.noreply.github.com>
2021-06-05 20:42:52 +0530

    is this correct?

--- pyrogram/types/bots_and_keyboards/keyboard_button_poll_type.py
+++ pyrogram/types/bots_and_keyboards/keyboard_button_poll_type.py
@@ -22,13 +22,13 @@ class KeyboardButtonPollType:
     which is allowed to be created and sent when the corresponding button is pressed.
     
     Parameters:
-        type (``str``):
+        type (``str``, *optional*):
             If quiz is passed, the user will be allowed to create only polls in the quiz mode.
             If regular is passed, only regular polls will be allowed.
             Otherwise, the user will be allowed to create a poll of any type.
     """
     def __init__(
         self,
-        type: str
+        type: str = ""
     ):
         self.type = type
