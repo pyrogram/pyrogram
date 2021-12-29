73237d30e079cea169f4227af5cb9c6c4e5bde81
SUBIN <64341611+subinps@users.noreply.github.com>
2021-12-29 18:35:44 +0530

    fix parsing send_as peer.

--- pyrogram/types/user_and_chats/chat.py
+++ pyrogram/types/user_and_chats/chat.py
@@ -322,10 +322,11 @@ class Chat(Object):
 
                 if isinstance(default_send_as, raw.types.PeerUser):
                     send_as_raw = users[default_send_as.user_id]
-                else:
+                    parsed_chat.send_as_chat = Chat._parse_chat(client, send_as_raw)
+                elif default_send_as:
                     send_as_raw = chats[default_send_as.channel_id]
+                    parsed_chat.send_as_chat = Chat._parse_chat(client, send_as_raw)
 
-                parsed_chat.send_as_chat = Chat._parse_chat(client, send_as_raw)
 
             if full_chat.pinned_msg_id:
                 parsed_chat.pinned_message = await client.get_messages(
