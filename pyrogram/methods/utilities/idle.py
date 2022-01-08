9081b61c89a1d921dc24dbcaffd36bfff7c528e3
Anton Kovalevich <kai3341@gmail.com>
2022-01-08 04:25:22 +0300

    Avoid busy-waiting antipattern

--- pyrogram/methods/utilities/idle.py
+++ pyrogram/methods/utilities/idle.py
@@ -23,13 +23,18 @@ from signal import signal as signal_fn, SIGINT, SIGTERM, SIGABRT
 
 log = logging.getLogger(__name__)
 
-is_idling = False
+is_idling: asyncio.Event = None
 
 # Signal number to name
 signals = {
     k: v for v, k in signal.__dict__.items()
     if v.startswith("SIG") and not v.startswith("SIG_")
 }
+
+
+def signal_handler(signum, __):
+    logging.info(f"Stop signal received ({signals[signum]}). Exiting...")
+    is_idling.set()
 
 
 async def idle():
@@ -70,17 +75,9 @@ async def idle():
             app3.stop()
     """
     global is_idling
-
-    def signal_handler(signum, __):
-        global is_idling
-
-        logging.info(f"Stop signal received ({signals[signum]}). Exiting...")
-        is_idling = False
+    is_idling = asyncio.Event()
 
     for s in (SIGINT, SIGTERM, SIGABRT):
         signal_fn(s, signal_handler)
 
-    is_idling = True
-
-    while is_idling:
-        await asyncio.sleep(1)
+    await is_idling.wait()
