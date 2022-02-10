84b0e15e2b06a9dac2b7f8755e1f7ac896bc9ee1
Dan <14043624+delivrance@users.noreply.github.com>
2022-02-10 20:14:09 +0100

    Revert "Make Connection.send() raise the actual exception"
    This reverts commit ed2db45a03dc9a2391fee8f40e9ff059c109c291.

--- pyrogram/connection/connection.py
+++ pyrogram/connection/connection.py
@@ -77,7 +77,10 @@ class Connection:
         log.info("Disconnected")
 
     async def send(self, data: bytes):
-        await self.protocol.send(data)
+        try:
+            await self.protocol.send(data)
+        except Exception:
+            raise OSError
 
     async def recv(self) -> Optional[bytes]:
         return await self.protocol.recv()
