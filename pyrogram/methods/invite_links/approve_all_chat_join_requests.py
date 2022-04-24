f6f6141b19c15c30cc2f83d5decab44f67b5818a
Dan <14043624+delivrance@users.noreply.github.com>
2022-04-24 11:56:07 +0200

    Add approve/decline_all_chat_join_requests

---
+++ pyrogram/methods/invite_links/approve_all_chat_join_requests.py
@@ -0,0 +1,53 @@
+#  Pyrogram - Telegram MTProto API Client Library for Python
+#  Copyright (C) 2017-present Dan <https://github.com/delivrance>
+#
+#  This file is part of Pyrogram.
+#
+#  Pyrogram is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU Lesser General Public License as published
+#  by the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  Pyrogram is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU Lesser General Public License for more details.
+#
+#  You should have received a copy of the GNU Lesser General Public License
+#  along with Pyrogram.  If not, see <http://www.gnu.org/licenses/>.
+
+from typing import Union
+
+import pyrogram
+from pyrogram import raw
+
+
+class ApproveAllChatJoinRequests:
+    async def approve_all_chat_join_requests(
+        self: "pyrogram.Client",
+        chat_id: Union[int, str],
+        invite_link: str = None
+    ) -> bool:
+        """Approve all pending join requests in a chat.
+
+        Parameters:
+            chat_id (``int`` | ``str``):
+                Unique identifier for the target chat or username of the target channel/supergroup
+                (in the format @username).
+
+            invite_link (``str``, *optional*):
+                Pass an invite link to approve only its join requests.
+                By default, all join requests are approved.
+
+        Returns:
+            ``bool``: True on success.
+        """
+        await self.invoke(
+            raw.functions.messages.HideAllChatJoinRequests(
+                peer=await self.resolve_peer(chat_id),
+                approved=True,
+                link=invite_link
+            )
+        )
+
+        return True
