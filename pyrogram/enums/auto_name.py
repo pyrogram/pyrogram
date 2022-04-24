5c0806a8a94079809c54899170fd4c3bfb652325
Dan <14043624+delivrance@users.noreply.github.com>
2022-04-24 11:56:07 +0200

    Add __repr__ to enumerations

--- pyrogram/enums/auto_name.py
+++ pyrogram/enums/auto_name.py
@@ -22,3 +22,6 @@ from enum import Enum
 class AutoName(Enum):
     def _generate_next_value_(self, *args):
         return self.lower()
+
+    def __repr__(self):
+        return f"pyrogram.enums.{self}"
