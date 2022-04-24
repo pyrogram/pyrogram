bd11767e887601f20f69378fc02da73eb3fc50f5
Dan <14043624+delivrance@users.noreply.github.com>
2022-04-24 11:56:07 +0200

    Use a shorter __license__ string

--- pyrogram/__init__.py
+++ pyrogram/__init__.py
@@ -17,7 +17,7 @@
 #  along with Pyrogram.  If not, see <http://www.gnu.org/licenses/>.
 
 __version__ = "1.4.16"
-__license__ = "GNU Lesser General Public License v3 or later (LGPLv3+)"
+__license__ = "GNU Lesser General Public License v3.0 (LGPL-3.0)"
 __copyright__ = "Copyright (C) 2017-present Dan <https://github.com/delivrance>"
 
 from concurrent.futures.thread import ThreadPoolExecutor
