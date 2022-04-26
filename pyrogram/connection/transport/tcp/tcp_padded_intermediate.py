7511a99c0f07d0b7e4f33d90bcbe2868ec07d320
Sam <100821827+01101sam@users.noreply.github.com>
2022-04-26 21:46:30 +0800

    ^ Update wrong NOTICE

--- pyrogram/connection/transport/tcp/tcp_padded_intermediate.py
+++ pyrogram/connection/transport/tcp/tcp_padded_intermediate.py
@@ -1,20 +1,20 @@
-# MTPager - A MITM server/forwarder based on Pyrogram framework
-# Copyright (C) 2022-present Sam <https://gitlab.com/sam01101>
+#  Pyrogram - Telegram MTProto API Client Library for Python
+#  Copyright (C) 2017-present Dan <https://github.com/delivrance>
 #
-# This file is part of MTPager.
+#  This file is part of Pyrogram.
 #
-# MTPager is free software: you can redistribute it and/or modify
-# it under the terms of the GNU Lesser General Public License as published
-# by the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
+#  Pyrogram is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU Lesser General Public License as published
+#  by the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
 #
-# MTPager is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU Lesser General Public License for more details.
+#  Pyrogram is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU Lesser General Public License for more details.
 #
-# You should have received a copy of the GNU Lesser General Public License
-# along with MTPager. If not, see <http://www.gnu.org/licenses/>.
+#  You should have received a copy of the GNU Lesser General Public License
+#  along with Pyrogram.  If not, see <http://www.gnu.org/licenses/>.
 
 from struct import pack, unpack
 from typing import Optional
