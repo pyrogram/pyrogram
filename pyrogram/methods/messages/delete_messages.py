6e1425ada3790b3ade70bacf21d968582d3882bf
DevOps117 <55235206+devops117@users.noreply.github.com>
2022-05-14 12:28:30 +0530

    Drop support for iterators where they are not needed (#969)
    * delete_messages: Drop support for generators
    
    Since we used a list there anyway, this approach will lead to more localized errors and can reduce function overhead.
    
    Signed-off-by: devops117 <55235206+devops117@users.noreply.github.com>
    
    * delete_messages: Return pts_count:int
    
    An example usecase would be for a normal bot which uses range based on message ids
    instead of keeping a track of messages and using the DeletedMessagesHandler.
    
    Signed-off-by: devops117 <55235206+devops117@users.noreply.github.com>
    
    * Drop support for Iterators and update docstrings and some cleanups.
    
    Signed-off-by: devops117 <55235206+devops117@users.noreply.github.com>
    
    * Update get_users.py
    
    * Update get_messages.py
    
    * Update delete_messages.py
    
    * Update forward_messages.py
    
    * Update get_messages.py
    
    Co-authored-by: Dan <14043624+delivrance@users.noreply.github.com>

--- pyrogram/methods/messages/delete_messages.py
+++ pyrogram/methods/messages/delete_messages.py
@@ -16,7 +16,7 @@
 #  You should have received a copy of the GNU Lesser General Public License
 #  along with Pyrogram.  If not, see <http://www.gnu.org/licenses/>.
 
-from typing import Union, Iterable
+from typing import Union, List
 
 import pyrogram
 from pyrogram import raw
@@ -26,9 +26,9 @@ class DeleteMessages:
     async def delete_messages(
         self: "pyrogram.Client",
         chat_id: Union[int, str],
-        message_ids: Union[int, Iterable[int]],
+        message_ids: Union[int, List[int]],
         revoke: bool = True
-    ) -> bool:
+    ) -> int:
         """Delete messages, including service messages.
 
         Parameters:
@@ -37,9 +37,8 @@ class DeleteMessages:
                 For your personal cloud (Saved Messages) you can simply use "me" or "self".
                 For a contact that exists in your Telegram address book you can use his phone number (str).
 
-            message_ids (``int`` | ``Iterable[int]``):
+            message_ids (``int`` | List of ``int``):
                 A list of Message identifiers to delete (integers) or a single message id.
-                Iterators and Generators are also accepted.
 
             revoke (``bool``, *optional*):
                 Deletes messages on both parts.
@@ -48,7 +47,7 @@ class DeleteMessages:
                 Defaults to True.
 
         Returns:
-            ``bool``: True on success, False otherwise.
+            ``int``: Amount of affected messages
 
         Example:
             .. code-block:: python
@@ -63,7 +62,9 @@ class DeleteMessages:
                 await app.delete_messages(chat_id, message_id, revoke=False)
         """
         peer = await self.resolve_peer(chat_id)
-        message_ids = list(message_ids) if not isinstance(message_ids, int) else [message_ids]
+        # Follow type annotation of the raw function "DeleteMessage".
+        if not isinstance(message_ids, list):
+            message_ids = [message_ids]
 
         if isinstance(peer, raw.types.InputPeerChannel):
             r = await self.invoke(
@@ -76,10 +77,10 @@ class DeleteMessages:
             r = await self.invoke(
                 raw.functions.messages.DeleteMessages(
                     id=message_ids,
-                    revoke=revoke or None
+                    revoke=revoke or None # Follow the type annotation.
                 )
             )
 
-        # Deleting messages you don't have right onto, won't raise any error.
+        # Deleting messages you don't have right onto won't raise any error.
         # Check for pts_count, which is 0 in case deletes fail.
-        return bool(r.pts_count)
+        return r.pts_count
