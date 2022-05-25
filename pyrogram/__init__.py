f7c678855d051e69974d50318bdcb30526ccdfcf
Dan <14043624+delivrance@users.noreply.github.com>
2022-05-25 10:58:32 +0200

    Update Pyrogram to v2.0.25

--- pyrogram/__init__.py
+++ pyrogram/__init__.py
@@ -16,7 +16,7 @@
 #  You should have received a copy of the GNU Lesser General Public License
 #  along with Pyrogram.  If not, see <http://www.gnu.org/licenses/>.
 
-__version__ = "2.0.24"
+__version__ = "2.0.25"
 __license__ = "GNU Lesser General Public License v3.0 (LGPL-3.0)"
 __copyright__ = "Copyright (C) 2017-present Dan <https://github.com/delivrance>"
 
