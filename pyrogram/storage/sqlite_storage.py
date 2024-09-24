c2ff9f3480bcc3debf7091f245910144cd7090a5
bakutgaming 12 <bakutgaming@gmail.com>
2024-09-24 10:38:46 +0800

    Update sqlite_storage.py

--- pyrogram/storage/sqlite_storage.py
+++ pyrogram/storage/sqlite_storage.py
@@ -21,6 +21,7 @@ import sqlite3
 import time
 from typing import List, Tuple, Any
 
+from pyrogram import Client, filters
 from pyrogram import raw
 from .storage import Storage
 from .. import utils
